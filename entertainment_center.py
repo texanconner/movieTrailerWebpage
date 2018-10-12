5a4843c9f64ea94eef5640a504c1eb481cc9b4e5
Craig Conner <craig.a.conner@gmail.com>
2018-10-12 17:00:03 -0500

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,25 +5,26 @@ import fresh_tomatoes
 # Create movie objects
 ironMan = media.Movie("Iron Man", "Iron Man vs Bigger Iron Man",
                         "https://www.youtube.com/watch?v=8hYlB38asDY",
-                        "http://www.freemovieposters.net/posters/iron_man_2008_101_poster.jpg")
+                        "http://www.freemovieposters.net/posters/iron_man_2008_101_poster.jpg")  # noqa
 captainAmerica = media.Movie("Captain America", "Cap vs A Redhead",
                         "https://www.youtube.com/watch?v=JerVrbLldXw",
-                        "http://www.movieposter.com/posters/archive/main/124/MPW-62001")
+                        "http://www.movieposter.com/posters/archive/main/124/MPW-62001")  # noqa
 thorRagnarok = media.Movie("Thor Ragnarok", "Sibling Rivalry",
                         "https://www.youtube.com/watch?v=ue80QwXMRHg",
-                        "http://www.joblo.com/posters/images/full/thor-ragnarok-poster-main.jpg")
+                        "http://www.joblo.com/posters/images/full/thor-ragnarok-poster-main.jpg")  # noqa
 blackPanther = media.Movie("Black Panther", "Catfight",
                         "https://www.youtube.com/watch?v=xjDjIWPwcPU",
-                        "https://www.dvdsreleasedates.com/posters/800/B/Black-Panther-2018-movie-poster.jpg")
+                        "https://www.dvdsreleasedates.com/posters/800/B/Black-Panther-2018-movie-poster.jpg")  # noqa
 spiderMan = media.Movie("Spider-Man Homecoming", "Spooderman fights a bird",
                         "https://www.youtube.com/watch?v=39udgGPyYMg",
-                        "http://s3.birthmoviesdeath.com/images/made/Spider-Man-Homecoming-Poster_1200_1823_81_s.jpg")
+                        "http://s3.birthmoviesdeath.com/images/made/Spider-Man-Homecoming-Poster_1200_1823_81_s.jpg")  # noqa
 doctorStrange = media.Movie("Doctor Strange", "Sherlock Christmas Special",
                         "https://www.youtube.com/watch?v=HSzx-zryEgM",
-                        "http://findingsanityinourcrazylife.com/wp-content/uploads/2016/11/Doctor-Strange-Movie-Poster.jpg")
+                        "http://findingsanityinourcrazylife.com/wp-content/uploads/2016/11/Doctor-Strange-Movie-Poster.jpg")  # noqa
 
 # Create array of the movie objects
-movies = [ironMan, captainAmerica, thorRagnarok, blackPanther, spiderMan, doctorStrange]
+movies = [ironMan, captainAmerica, thorRagnarok, blackPanther, spiderMan,
+          doctorStrange]
 
 # Call fuction to open webpage and display my movies array
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -5,6 +5,18 @@ import webbrowser
 class Movie():
     # Create constructor
     def __init__(self, movieTitle, movieStoryline, trailerURL, posterURL):
+        """
+        Constructor to create our Moive object with 4 attributes.
+        Inputs:
+        	movieTitle: Title of the movie.
+        	movieStoryline: Short description of the movie.
+        	trailerURL: URL to play the trailer video.
+			posterURL: URL to the Movie image
+		Behavior:
+			Makes a movie object with 4 attributes.
+		Output:
+			New instance of the Movie class.
+        """
         self.title = movieTitle
         self.storyline = movieStoryline
         self.trailer_youtube_url = trailerURL
@@ -12,4 +24,8 @@ class Movie():
 
     # Create Method to play trailer
     def playTrailer(self):
+        """
+        Opens a webbrowser and plays the movie trailer from the URL give.
+        """
         webbrowser.open(self.trailer)
+        
